4902b4a5967d5766e325afd32457cf44acbbc730
Philip Stathis <1742750+philipstathis@users.noreply.github.com>
2020-07-02 16:23:43 +0000

    add ag grid for react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: [{
+        headerName: "First Name", field: "profile.first_name", sortable: true, filter: true
+      }, {
+        headerName: "Officer Role Selected", field: "answers.role"
+      },{
+        headerName: "Toastmasters Club Name", field: "answers.clubName"
+      },{
+        headerName: "Toastmasters Club Number", field: "answers.clubNumber"
+      }, {
+        headerName: "Confirmed", field: "checked_in"
+      }],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://a5slwb8wx6.execute-api.us-east-1.amazonaws.com/dev/events')
+    .then(result => result.json())
+    .then(result => result["attendees"])
+    .then(result => result.map(attendee => {
+      let answerObjects = attendee.answers;
+      let roleAnswer = answerObjects.find(obj => {
+        return obj["question_id"] === "34234268";
+      });
+      if (roleAnswer) {
+        answerObjects["role"] = roleAnswer["answer"]
+      }
+      let clubName = answerObjects.find(obj => {
+        return obj["question_id"] === "34370854";
+      });
+      if (clubName) { answerObjects["clubName"] = clubName["answer"] }
+      // else { answerObjects["clubName"] = '';}
+      let clubNumber = answerObjects.find(obj => {
+        return obj["question_id"] === "34370856";
+      });
+      if (clubNumber) { answerObjects["clubNumber"] = clubNumber["answer"] }
+      // else { answerObjects["clubNumber"] = '';}
+      return attendee;
+    }))
+    .then(rowData => this.setState({rowData}))
+  }
+
+  render() {
+    return (
+      <div
+        className="ag-theme-alpine"
+        style={{
+        height: '500px',
+        width: '1000px' 
+      }
+      }
+      >
+        <AgGridReact
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData}>
+        </AgGridReact>
+      </div>
+    );
+  }
 }
 
 export default App;
